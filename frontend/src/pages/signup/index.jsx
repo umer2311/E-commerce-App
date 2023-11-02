69459097d68a0f27a52a1139ccd18686ba3d7bc7
anbreenshakrullah <shakrullah@gmail.com>
2023-11-02 13:12:30 +0500

    login_signup

---
+++ frontend/src/pages/signup/index.jsx
@@ -0,0 +1,102 @@
+import React, { useState } from 'react';
+
+const SignUp = () => {
+  const [name, setName] = useState('');
+  const [email, setEmail] = useState('');
+  const [contactNumber, setContactNumber] = useState('');
+  const [role, setRole] = useState('user');
+
+  const handleSignUp = (e) => {
+    e.preventDefault();
+    // You can add your Sign Up logic here
+    console.log('Name:', name);
+    console.log('Email:', email);
+    console.log('Contact Number:', contactNumber);
+    console.log('Role:', role);
+  };
+
+  return (
+    <div className="min-h-screen flex items-center justify-center bg-gray-50 py-12 px-4 sm:px-6 lg:px-8">
+      <div className="max-w-md w-full space-y-8">
+        <div>
+          <h2 className="mt-6 text-center text-3xl font-extrabold text-red-400">Sign Up for an Account</h2>
+        </div>
+        <form className="mt-8 space-y-6" onSubmit={handleSignUp}>
+          <input type="hidden" name="remember" value="true" />
+          <div className="space-y-4">
+            <div>
+              <label htmlFor="name" className="sr-only">Name</label>
+              <input
+                id="name"
+                name="name"
+                type="text"
+                autoComplete="name"
+                required
+                value={name}
+                onChange={(e) => setName(e.target.value)}
+                className="rounded-md w-full px-3 py-2 border border-#FFAEAE placeholder-gray-500 text-gray-900 focus:outline-none focus:ring-indigo-500 focus:border-indigo-500 focus:z-10 sm:text-sm"
+                placeholder="Name"
+              />
+            </div>
+            <div>
+              <label htmlFor="email" className="sr-only">Email</label>
+              <input
+                id="email"
+                name="email"
+                type="email"
+                autoComplete="email"
+                required
+                value={email}
+                onChange={(e) => setEmail(e.target.value)}
+                className="rounded-md w-full px-3 py-2 border border-#FFAEAE placeholder-gray-500 text-gray-900 focus:outline-none focus:ring-indigo-500 focus:border-indigo-500 focus:z-10 sm:text-sm"
+                placeholder="Email"
+              />
+            </div>
+            <div>
+              <label htmlFor="contactNumber" className="sr-only">Contact Number</label>
+              <input
+                id="contactNumber"
+                name="contactNumber"
+                type="tel"
+                autoComplete="tel"
+                required
+                value={contactNumber}
+                onChange={(e) => setContactNumber(e.target.value)}
+                className="rounded-md w-full px-3 py-2 border border-#FFAEAE placeholder-gray-500 text-gray-900 focus:outline-none focus:ring-indigo-500 focus:border-indigo-500 focus:z-10 sm:text-sm"
+                placeholder="Contact Number"
+              />
+            </div>
+            <div className="relative rounded-md shadow-sm">
+              <select
+                id="role"
+                name="role"
+                value={role}
+                onChange={(e) => setRole(e.target.value)}
+                className="block w-full pl-3 pr-10 py-2 border border-#FFAEAE rounded-md bg-white text-gray-900 focus:outline-none focus:ring-indigo-500 focus:border-indigo-500 focus:z-10 sm:text-sm"
+              >
+                <option value="user">User</option>
+                <option value="admin">Admin</option>
+              </select>
+            </div>
+          </div>
+          <div>
+            <button
+              type="submit"
+              className="group relative w-full flex justify-center py-2 px-4 border border-transparent text-sm font-medium rounded-md text-white bg-gradient-to-r from-red-300 to-red-400 hover:from-red-400 hover:to-red-300 focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-red-500"
+            >
+              Sign Up
+            </button>
+          </div>
+          <div className="text-sm text-center">
+            Already have an account?{' '}
+            <a href="#" className="font-medium text-red-400 hover:text-red-500">
+              Log In
+            </a>
+          </div>
+        </form>
+      </div>
+    </div>
+  );
+};
+
+export default SignUp;
