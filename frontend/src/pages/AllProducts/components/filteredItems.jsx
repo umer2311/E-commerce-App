b78385ee57e3d7211f8b790c18beee1bf9b572f1
j-awwad <jawwad.amd@gmail.com>
2023-11-12 21:19:08 +0500

    filter functionality added

--- frontend/src/pages/AllProducts/components/filteredItems.jsx
+++ frontend/src/pages/AllProducts/components/filteredItems.jsx
@@ -1,27 +1,18 @@
 import React from "react";
+import ProductCard from "../../homepage/compoments/productCard";
+import { products } from "../../../assets/dummyData";
 
 export const FilteredItems = () => {
   return (
     <div>
-      {/* <div className="grid grid-rows-6  lg:grid-cols-3 lg:gap-3 w-2/3 h-96  mt-8"></div> */}
-
-      {/* filter */}
-      <div className="grid grid-rows-6  lg:grid-cols-3 lg:gap-3 w-2/3 h-96  mt-8">
-        {/* <div className="bredcrum col-span-4">
-          <p>Home</p>
+      <div className="container mx-auto p-8">
+        <div className="grid md:grid-cols-2 lg:grid-cols-3 gap-8">
+          {products?.map((product, index) => (
+            <div key={index} className="">
+              <ProductCard product={product} />
+            </div>
+          ))}
         </div>
-
-        <div className="productName col-span-4">
-          <p>Selected product</p>
-        </div>
-
-        <div className="filterTabs col-span-4">
-          <p>for now empty</p>
-        </div>
-
-        <div className="mainFilter row-span-6 ">
-          <h2>fs</h2>
-        </div> */}
       </div>
     </div>
   );
