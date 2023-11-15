29fd3fecbcb8b28fe9f9ee0893be2b1e9633811c
j-awwad <jawwad.amd@gmail.com>
2023-11-15 15:18:52 +0500

    content added in about us page

--- frontend/src/pages/aboutUs/index.jsx
+++ frontend/src/pages/aboutUs/index.jsx
@@ -18,6 +18,9 @@ import {
 import Umer from "../../assets/images/umer.jpg";
 import Jawwad from "../../assets/images/jawwad.jpeg";
 import Profile from "../../assets/images/profile.png";
+import Story from "../../assets/images/story3.svg";
+import Img from "../../assets/images/img-11.png";
+import Mission from "../../assets/images/mission.jpg";
 
 function AboutUs() {
   return (
@@ -89,12 +92,53 @@ function AboutUs() {
         </div>
       </div>
 
-      {/* Story */}
       <div className=" py-2 grid md:grid-cols-2">
-        <div className="w-full rounded-r-sm bg-gray-200"></div>
+        {/* mission */}
+
+        <div className="left w-full  bg-red-100 p-10  overflow-scroll lg:overflow-hidden ">
+          <h2 className="text-2xl font-bold text-black mb-4">Our Mission</h2>
+          <p className="text-slate-500 text-lg ">
+            We are a company that values integrity, quality, and community. Our
+            team is dedicated to providing the best products and services to our
+            customers. We believe that by working together, we can achieve more
+            than we could alone.We are a company that values integrity, quality,
+            and community. Our team is dedicated to providing the best products
+            and services to our customers. We believe that by working together,
+            we can achieve more than we could alone.
+          </p>
+        </div>
+        <div className="right w-full rounded-r-sm bg-gray-200">
+          <img
+            src={Mission}
+            alt="img"
+            className="object-contain w-full h-full"
+          />
+          {/* <div className="bg-red-200 w-3/6 h-5/6 mx-auto py-3 rounded-lg my-9">
+            <h1 className="text-center">Enriched products</h1>
+            <div className="bg-gray-200 h-80  ">
+              <div className="h-5/6 relative">
+                <img
+                  src={Mission}
+                  alt="img"
+                  className="object-cover w-full h-full"
+                />
+              </div>
+
+              <div className=" bg-white h-1/6  text-center">
+                <p>Enriched Serum & </p>
+                <p>Founder, MAC</p>
+              </div>
+            </div>
+          </div> */}
+        </div>
+
+        {/* story */}
+        <div className="w-full rounded-r-sm bg-gray-200">
+          <img src={Story} alt="img" className="object-contain w-full h-full" />
+        </div>
 
         <div className=" w-full h-96 bg-red-100 p-10  overflow-scroll lg:overflow-hidden ">
-          <h2 className="text-2xl font-bold text-black mb-4">Our Story</h2>
+          {/* <h2 className="text-2xl font-bold text-black mb-4">Our Story</h2> */}
           <p className="text-slate-500 text-lg ">
             We are a company that values integrity, quality, and community. Our
             team is dedicated to providing the best products and services to our
@@ -102,7 +146,13 @@ function AboutUs() {
             than we could alone. Lorem ipsum dolor sit amet, consectetur
             adipisicing elit. Temporibus, sint labore provident voluptatum
             libero dignissimos assumenda odit repellendus nisi aspernatur ullam.
-            Veritat\
+            Veritat\Lorem ipsum dolor sit amet consectetur adipisicing elit.
+            Eveniet magnam culpa aliquam, nesciunt est ipsum facilis quam
+            sapiente qui tenetur quis voluptatum. Lorem ipsum dolor sit amet,
+            consectetur adipisicing elit. Vero voluptatem provident doloribus
+            pariatur facilis dolor ut quis eligendi molestias. Eveniet
+            repudiandae officiis ea temporibus distinctio asperiores modi odit
+            necessitatibus minus? Ad, ea illo?
           </p>
         </div>
 
